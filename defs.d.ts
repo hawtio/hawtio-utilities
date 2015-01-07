916e9ad01fb5434b18ee45a34affda7587acc4bf
Stan Lewis <slewis@fusesource.com>
2015-01-07 16:23:15 -0500

    Initial spike of hawtio-utilities

---
+++ defs.d.ts
@@ -0,0 +1,17 @@
+/// <reference path="d.ts/stringHelpers.d.ts"/>
+/// <reference path="d.ts/coreInterfaces.d.ts"/>
+/// <reference path="d.ts/includes.d.ts"/>
+/// <reference path="d.ts/arrayHelpers.d.ts"/>
+/// <reference path="d.ts/urlHelpers.d.ts"/>
+/// <reference path="d.ts/baseHelpers.d.ts"/>
+/// <reference path="d.ts/controllerHelpers.d.ts"/>
+/// <reference path="d.ts/tasks.d.ts"/>
+/// <reference path="d.ts/coreHelpers.d.ts"/>
+/// <reference path="d.ts/fileUploadHelpers.d.ts"/>
+/// <reference path="d.ts/filterHelpers.d.ts"/>
+/// <reference path="d.ts/jolokiaInterfaces.d.ts"/>
+/// <reference path="d.ts/objectHelpers.d.ts"/>
+/// <reference path="d.ts/pluginHelpers.d.ts"/>
+/// <reference path="d.ts/pollHelpers.d.ts"/>
+/// <reference path="d.ts/selectionHelpers.d.ts"/>
+/// <reference path="d.ts/storageHelpers.d.ts"/>
