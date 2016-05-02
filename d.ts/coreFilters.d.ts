84991f2239b482379fe5f3c323e895773a8eea45
Stan Lewis <gashcrumb@gmail.com>
2016-05-02 16:15:00 -0400

    Add humanizeDate/relativeTime/humanizeDuration filters

--- d.ts/coreFilters.d.ts
+++ d.ts/coreFilters.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="coreHelpers.d.ts" />
+declare var humanizeDuration: any;
+declare var humandate: any;
 declare module CoreFilters {
 }
--- helpers/coreFilters.ts
+++ helpers/coreFilters.ts
@@ -1,4 +1,8 @@
 /// <reference path="coreHelpers.ts" />
+
+declare var humanizeDuration:any;
+declare var humandate:any;
+
 module CoreFilters {
   var pluginName = 'hawtio-core-filters';
   var _module = angular.module(pluginName, []);
@@ -7,6 +11,30 @@ module CoreFilters {
   _module.filter('humanize', () => Core.humanizeValue);
   _module.filter('humanizeMs', () => Core.humanizeMilliseconds);
   _module.filter('maskPassword', () => Core.maskPassword);
+
+  // relativeTime was the first humanize filter for dates,
+  // let's maybe also add a 'humanizeDate' filter to match
+  // up with 'humanizeDuration'
+  var relativeTimeFunc = (date) => {
+    return humandate.relativeTime(date);
+  };
+
+  // Turn a date into a relative time from right now
+  _module.filter('relativeTime', () => {
+    return relativeTimeFunc;
+  });
+
+  _module.filter('humanizeDate', () => {
+    return relativeTimeFunc;
+  });
+
+  // Output a duration in milliseconds in a human-readable format
+  _module.filter('humanizeDuration', () => {
+    return (duration) => {
+      return humanizeDuration(duration, { round: true });
+    }
+  });
+
 
   hawtioPluginLoader.addModule(pluginName);
 
